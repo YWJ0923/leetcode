e7e9a46ee5282d8ce791f01404d281b8a78b54ae
ywj <694343674@qq.com>
2021-09-21 19:05:47 +0800

    2021.9.21

---
+++ Java/Offer 06.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            res[i++] = stack.poll();
+        }
+        return res;
+    }
+}
--- Java/Offer 53_1.java
+++ Java/Offer 53_1.java
@@ -15,3 +15,17 @@ class Solution {
         return count;
     }
 }
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int count = 0;
+        int index = Arrays.binarySearch(nums, target);
+        for (int i = index; i >= 0 && i < nums.length && nums[i] == target; i++) {
+            count++;
+        }
+        for (int i = index - 1; i >= 0 && i < nums.length && nums[i] == target; i--) {
+            count++;
+        }
+        return count;
+    }
+}
