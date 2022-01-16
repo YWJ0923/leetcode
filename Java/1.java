b05ede11e94804e58c156f506857f32e7ee5f413
YWJ0923 <694343674@qq.com>
2022-01-16 22:30:26 +0800

    2022.1.16

--- Java/1.java
+++ Java/1.java
@@ -24,3 +24,20 @@ class Solution {
         return new int[0];
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                int[] ans = new int[2];
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                return ans;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
