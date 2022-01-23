ce55acc860aa8dca1d59de31dd566b4916077547
ywj <694343674@qq.com>
2022-01-23 16:51:28 +0800

    2022.1.23

--- Java/11.java
+++ Java/11.java
@@ -33,3 +33,19 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            ans = Math.max(ans, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return ans;
+    }
+}
